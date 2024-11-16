63eb66c514964da76d5826bb5ae049bd5bd7b313
LanzoMA <lanzo.ma9@gmail.com>
2024-11-16 13:09:28 +0000

    outlined login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:sprinter_mobile/components/sprinter_text_field.dart';
+import 'package:sprinter_mobile/pages/home_page.dart';
+import 'package:sprinter_mobile/pages/sign_up_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void login() {
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) return;
+
+    // HTTP request
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const HomePage(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Login Page'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Login'),
+            const SizedBox(height: 16),
+            SprinterTextField(
+              hintText: 'Email',
+              controller: emailController,
+            ),
+            const SizedBox(height: 16),
+            SprinterTextField(
+              hintText: 'Password',
+              controller: passwordController,
+              obscureText: true,
+            ),
+            TextButton(
+              onPressed: login,
+              child: const Text('Login'),
+            ),
+            Row(
+              children: [
+                const Text('No account? '),
+                InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const SignUpPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
