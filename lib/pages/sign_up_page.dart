6ff5dc43f938ce43f841a17feb310c851d832119
LanzoMA <lanzo.ma9@gmail.com>
2024-11-17 16:00:08 +0000

    added error messages

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -17,20 +17,51 @@ class _SignUpPageState extends State<SignUpPage> {
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
 
+  String emailErrorMessage = '';
+  String usernameErrorMessage = '';
+  String passwordErrorMessage = '';
+  String confirmPasswordErrorMessage = '';
+
   void signUp() async {
+    setState(() {
+      emailErrorMessage = usernameErrorMessage =
+          passwordErrorMessage = confirmPasswordErrorMessage = '';
+    });
+
     final String email = emailController.text.trim();
     final String username = usernameController.text.trim();
     final String password = passwordController.text.trim();
     final String confirmPassword = confirmPasswordController.text.trim();
+
+    if (email.isEmpty) emailErrorMessage = 'No email is provided';
+    if (username.isEmpty) usernameErrorMessage = 'No username is provided';
+    if (password.isEmpty) passwordErrorMessage = 'No password is provided';
+    if (confirmPassword.isEmpty)
+      confirmPasswordErrorMessage = 'No password is provided';
 
     final bool isAnyFieldEmpty = email.isEmpty ||
         username.isEmpty ||
         password.isEmpty ||
         confirmPassword.isEmpty;
 
-    if (isAnyFieldEmpty) return;
-    if (password != confirmPassword) return;
-    if (password.length < 8) return;
+    if (isAnyFieldEmpty) {
+      setState(() {});
+      return;
+    }
+
+    if (password != confirmPassword) {
+      setState(() {
+        confirmPasswordErrorMessage = 'Passwords do not match';
+      });
+      return;
+    }
+
+    if (password.length < 8) {
+      setState(() {
+        passwordErrorMessage = 'Password is less than 8 characters';
+      });
+      return;
+    }
 
     final Map<String, String> data = {
       'email': email,
@@ -53,6 +84,9 @@ class _SignUpPageState extends State<SignUpPage> {
       );
     } catch (error) {
       print(error);
+      setState(() {
+        emailErrorMessage = 'Account already registed with this email';
+      });
     }
   }
 
@@ -61,57 +95,108 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Sign Up'),
-            const SizedBox(height: 16),
-            SprinterTextField(
-              hintText: 'Email',
-              controller: emailController,
-            ),
-            const SizedBox(height: 16),
-            SprinterTextField(
-              hintText: 'Username',
-              controller: usernameController,
-            ),
-            const SizedBox(height: 16),
-            SprinterTextField(
-              hintText: 'Password',
-              obscureText: true,
-              controller: passwordController,
-            ),
-            const SizedBox(height: 16),
-            SprinterTextField(
-              hintText: 'Confirm Password',
-              obscureText: true,
-              controller: confirmPasswordController,
-            ),
-            TextButton(
-              onPressed: signUp,
-              child: const Text('Sign Up'),
-            ),
-            Row(
-              children: [
-                const Text('Already have an account? '),
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const LoginPage(),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Sign Up'),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SprinterTextField(
+                      hintText: 'Email',
+                      controller: emailController,
                     ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  Visibility(
+                    visible: emailErrorMessage.isNotEmpty,
+                    child: Text(
+                      emailErrorMessage,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SprinterTextField(
+                      hintText: 'Username',
+                      controller: usernameController,
+                    ),
+                  ),
+                  Visibility(
+                    visible: usernameErrorMessage.isNotEmpty,
+                    child: Text(
+                      usernameErrorMessage,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SprinterTextField(
+                      hintText: 'Password',
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                  ),
+                  Visibility(
+                    visible: passwordErrorMessage.isNotEmpty,
+                    child: Text(
+                      passwordErrorMessage,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: SprinterTextField(
+                      hintText: 'Confirm Password',
+                      obscureText: true,
+                      controller: confirmPasswordController,
+                    ),
+                  ),
+                  Visibility(
+                    visible: confirmPasswordErrorMessage.isNotEmpty,
+                    child: Text(
+                      confirmPasswordErrorMessage,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.error,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: signUp,
+                    child: const Text('Sign Up'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Already have an account? '),
+                      InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,6 +2,6 @@ import 'package:dio/dio.dart';
 
 const host = 'http://192.168.223.200';
 const port = '5000';
-const url = '${host}:${port}';
+const url = '$host:$port';
 
 final Dio dio = Dio();
