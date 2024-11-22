35588acad5b0b43e3fff639f9181e36bb66486b1
LanzoMA <lanzo.ma9@gmail.com>
2024-11-22 22:10:32 +0000

    added change email page

--- lib/pages/settings/change_email_page.dart
+++ lib/pages/settings/change_email_page.dart
@@ -1,15 +1,117 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sprinter_mobile/components/sprinter_button.dart';
+import 'package:sprinter_mobile/components/sprinter_text_field.dart';
+import 'package:sprinter_mobile/utils/dio.dart';
+import 'package:sprinter_mobile/utils/secure_storage.dart';
 
-class ChangeEmailPage extends StatelessWidget {
+class ChangeEmailPage extends StatefulWidget {
   const ChangeEmailPage({super.key});
+
+  @override
+  State<ChangeEmailPage> createState() => _ChangeEmailPageState();
+}
+
+class _ChangeEmailPageState extends State<ChangeEmailPage> {
+  final TextEditingController newEmailController = TextEditingController();
+
+  String? newEmailErrorText;
+
+  @override
+  void initState() {
+    super.initState();
+    newEmailController.addListener(
+      () => setState(
+        () => newEmailErrorText = null,
+      ),
+    );
+  }
+
+  void changeEmail() async {
+    setState(() {
+      newEmailErrorText = null;
+    });
+
+    final newEmail = newEmailController.text.trim();
+
+    if (newEmail.isEmpty) {
+      setState(() {
+        newEmailErrorText = 'New email not provided';
+      });
+    }
+
+    if (!newEmail.contains('@') || !newEmail.contains('.')) {
+      setState(() {
+        newEmailErrorText = 'Please enter a valid email address';
+      });
+    }
+
+    final Map<String, String> data = {
+      'email': newEmail,
+    };
+
+    try {
+      final String? accessToken = await getAccessToken();
+
+      if (accessToken == null) return;
+
+      final response = await dio.put(
+        '/account/email',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+          },
+        ),
+        data: data,
+      );
+
+      final newAccessToken = response.data['accessToken'];
+
+      await storeAccessToken(newAccessToken);
+
+      context.pop();
+    } on DioException catch (error) {
+      if (error.response != null && error.response?.statusCode == 400) {
+        setState(() {
+          newEmailErrorText = error.response?.data;
+        });
+        return;
+      }
+
+      setState(() {
+        newEmailErrorText = 'Server currently down. Please try again later.';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: const [
-          Text('Change Email'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 16),
+            const Text(
+              'Change Email',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+            ),
+            const SizedBox(height: 16),
+            SprinterTextField(
+              hintText: 'New Email',
+              controller: newEmailController,
+              icon: Icons.email,
+              errorText: newEmailErrorText,
+            ),
+            const SizedBox(height: 16),
+            SprinterButton(
+              onTap: changeEmail,
+              data: 'Change Email',
+            ),
+          ],
+        ),
       ),
     );
   }
